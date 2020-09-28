337da0e06857aa27860d0b130bf2241162073ce9
Mohammed Ali P S <48435733+psmohammedali@users.noreply.github.com>
2020-09-28 20:25:36 +0530

    Create Main.java

---
+++ coreBasics/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main
+{
+    String name="ali";
+    double myMoney = 123.5;
+    static String def;
+
+    public static void main(String[] args)
+    {
+        def = "hiii";
+        Main m = new Main();
+        Main s = new Main();
+        System.out.println(def);
+
+        System.out.println(def);
+
+    }
+}
+
